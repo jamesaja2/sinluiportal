37d3b7b40427acc7ccb6b4e1c596d1d6257468b8
James Timothy <u30894@s.smakstlouis1sby.sch.id>
2025-01-15 11:12:16 +0700

    Update Static Link Lists

--- src/data/staticLinks.ts
+++ src/data/staticLinks.ts
@@ -7,6 +7,7 @@ import scbt from '../assets/scbt.png';
 import twofa from '../assets/2fa.png';
 import canva from '../assets/canva.png';
 import email from '../assets/email.png';
+import websklh from '../assets/logo.png'
 
 // Public links (not logged in)
 const publicLinks: LinkCategories = [
@@ -15,6 +16,7 @@ const publicLinks: LinkCategories = [
     name: "Applications and Services",
     lexorank: "a",
     links: [
+      
       {
         title: 'Sinlui Learning Center',
         description: 'Portal pembelajaran digital',
@@ -22,11 +24,30 @@ const publicLinks: LinkCategories = [
         iconUrl: slc,
       },
       {
-        title: 'SiLuisa',
-        description: 'Sistem Informasi Saint Louis',
-        url: 'https://www.luisa.id/login/google/redirect/',
-        iconUrl: luisa,
-      }
+        title: 'Office 365',
+        description: 'Akses layanan Office 365',
+        url: 'https://accounts.google.com/o/saml2/initsso?idpid=C01jrcord&spid=936554538791',
+        iconUrl: office365,
+      },
+      {
+        title: 'e-Presensi',
+        description: 'Kehadiran di kelas',
+        url: 'https://absensi.stlouislc.net/',
+        iconUrl: presensi,
+      },
+      {
+        title: 'Sinlui Computer Based Test (SCBT)',
+        description: 'Akses ke soal penilaian',
+        url: 'sebs://smakstlouis1sby.sch.id/seb/config/PAS-Local.seb',
+        iconUrl: scbt,
+      },
+      {
+        title: 'Situs Resmi SMAK St. Louis 1 Surabaya',
+        description: 'Akses ke situs resmi',
+        url: 'https://smakstlouis1sby.sch.id/',
+        iconUrl: websklh,
+      },
+
     ]
   }
 ];
@@ -45,7 +66,7 @@ const studentLinks: LinkCategories = [
         iconUrl: slc,
       },
       {
-        title: 'SiLuisa',
+        title: 'Luisa',
         description: 'Cek biodata dan nilai',
         url: 'https://www.luisa.id/login/google/redirect/',
         iconUrl: luisa,
@@ -71,7 +92,7 @@ const studentLinks: LinkCategories = [
       {
         title: 'Email Siswa',
         description: 'Akses email @s.smakstlouis1sby.sch.id',
-        url: 'https://mail.google.com/',
+        url: 'https://accounts.google.com/AccountChooser?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser&ec=asw-gmail-globalnav-signin',
         iconUrl: email,
       },
       {
@@ -79,7 +100,13 @@ const studentLinks: LinkCategories = [
         description: 'Cek keamanan akun anda',
         url: 'https://myaccount.google.com/signinoptions/twosv',
         iconUrl: twofa,
-      }
+      },
+      {
+        title: 'Situs Resmi SMAK St. Louis 1 Surabaya',
+        description: 'Akses ke situs resmi',
+        url: 'https://smakstlouis1sby.sch.id/',
+        iconUrl: websklh,
+      },
     ]
   }
 ];
@@ -100,7 +127,7 @@ const staffLinks: LinkCategories = [
       {
         title: 'SiLuisa',
         description: 'Input nilai dan manajemen kelas',
-        url: 'https://www.luisa.id/login/google/redirect/',
+        url: 'https://si.luisa.id/login',
         iconUrl: luisa,
       },
       {
@@ -124,7 +151,7 @@ const staffLinks: LinkCategories = [
       {
         title: 'Email Staff',
         description: 'Akses email @smakstlouis1sby.sch.id',
-        url: 'https://mail.google.com/',
+        url: 'https://accounts.google.com/AccountChooser?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser&ec=asw-gmail-globalnav-signin',
         iconUrl: email,
       },
       {
